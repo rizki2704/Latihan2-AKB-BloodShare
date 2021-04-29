4dcf682f4fc55614fd4e6b098e459d68438c2e6e
resturr <restur2411@gmail.com>
2021-04-29 13:58:02 +0700

    Init commit

---
+++ app/src/androidTest/java/com/rizki/latihan2_bloodshare_if1_10118025/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.rizki.latihan2_bloodshare_if1_10118025
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.rizki.latihan2_bloodshare_if1_10118025", appContext.packageName)
+    }
+}
